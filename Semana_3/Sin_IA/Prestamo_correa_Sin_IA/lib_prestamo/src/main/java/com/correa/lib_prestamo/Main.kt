fc79af6c6a8909a0d925bee431b2d0af60f4a6d8
Fernando Correa Huincho <fernando.correa@tecsup.edu.pe>
2026-08-30 20:41:51 -0500

    Establecer entrada y salida de datos

--- Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Main.kt
+++ Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Main.kt
@@ -1,4 +1,19 @@
 package com.correa.lib_prestamo
 
 fun main() {
+    print("Nombre del producto: ")
+    val nombre = readLine()!!
+    print("Precio: ")
+    val precio = readLine()!!.toDouble()
+    print("Cantidad: ")
+    val cantidad = readLine()!!.toInt()
+
+    val producto = Producto(nombre, precio, cantidad)
+
+    print("Número de cuotas (6, 12, 24): ")
+    val numCuotas = readLine()!!.toInt()
+
+    val financiamiento = Financiamiento(producto, numCuotas)
+    financiamiento.mostrarResumen()
+    financiamiento.mostrarCronograma()
 }
