a14d1aa0364f2adea28dec6d5680498e7d965820
Fernando Correa Huincho <fernando.correa@tecsup.edu.pe>
2026-08-30 20:38:33 -0500

    Establecer clase Financiamiento, Interes y Funciones basicas

--- Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Financiamiento.kt
+++ Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Financiamiento.kt
@@ -1,2 +1,19 @@
 package com.correa.lib_prestamo
 
+import java.time.LocalDate
+
+class Financiamiento(
+    val producto: Producto,
+    val numCuotas: Int) {
+
+    private val interes: Double = when (numCuotas) {
+        6 -> 0.20
+        12 -> 0.40
+        24 -> 0.60
+        else -> 0.0
+    }
+
+    fun montoInteres(): Double = producto.montoTotal() * interes
+    fun montoAPagar(): Double = producto.montoTotal() + montoInteres()
+    fun pagoMensual(): Double = montoAPagar() / numCuotas
+}
