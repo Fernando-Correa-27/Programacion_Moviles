3d708686d300fd9ebeb6775c374d4ebf97476dc0
Fernando Correa Huincho <fernando.correa@tecsup.edu.pe>
2026-08-30 20:40:29 -0500

    Agregar funciones mostrarResumen y mostrarCronograma

--- Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Financiamiento.kt
+++ Semana_3/Sin_IA/Prestamo_correa_Sin_IA/lib_prestamo/src/main/java/com/correa/lib_prestamo/Financiamiento.kt
@@ -16,4 +16,35 @@ class Financiamiento(
     fun montoInteres(): Double = producto.montoTotal() * interes
     fun montoAPagar(): Double = producto.montoTotal() + montoInteres()
     fun pagoMensual(): Double = montoAPagar() / numCuotas
+
+    fun mostrarResumen() {
+        println("\nMonto inicial: ${"%.2f".format(producto.montoTotal())}")
+        println("Interes (${(interes * 100).toInt()}%): ${"%.2f".format(montoInteres())}")
+        println("Monto a pagar: ${"%.2f".format(montoAPagar())}")
+        println("Pago mensual: ${"%.2f".format(pagoMensual())}")
+    }
+
+    fun mostrarCronograma() {
+        var saldo = montoAPagar()
+        var fecha = LocalDate.now()
+
+        println("\n%-4s%-12s%-14s%-14s%-14s".format("N°", "Fecha", "Monto", "P.Mensual", "Resta Pago"))
+        for (i in 1..numCuotas) {
+            val saldoAntes = saldo
+            saldo -= pagoMensual()
+
+            if (Math.abs(saldo) < 0.01) saldo = 0.0
+
+            println(
+                "%-4d%-12s%-14s%-14s%-14s".format(
+                    i,
+                    fecha.toString(),
+                    "%.2f".format(saldoAntes),
+                    "%.2f".format(pagoMensual()),
+                    "%.2f".format(saldo)
+                )
+            )
+            fecha = fecha.plusMonths(1)
+        }
+    }
 }
