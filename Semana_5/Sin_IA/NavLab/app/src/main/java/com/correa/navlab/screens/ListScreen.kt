e899479815d1ce6f093d853c2d3f3cf767b437ca
Fernando Correa Huincho <fernando.correa@tecsup.edu.pe>
2026-09-23 02:57:46 -0500

    Implementar pantalla de lista de elementos

--- Semana_5/Sin_IA/NavLab/app/src/main/java/com/correa/navlab/screens/ListScreen.kt
+++ Semana_5/Sin_IA/NavLab/app/src/main/java/com/correa/navlab/screens/ListScreen.kt
@@ -1,2 +1,75 @@
 package com.correa.navlab.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.correa.navlab.navigation.Screen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListScreen(
+    navController: NavController
+) {
+    val items = List(8) { index ->
+        "Elemento ${index + 1}"
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Lista de elementos")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Volver"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+
+            items(items.size) { index ->
+
+                ListItem(
+                    headlineContent = {
+                        Text(items[index])
+                    },
+                    supportingContent = {
+                        Text("ID: ${index + 1}")
+                    },
+                    modifier = Modifier
+                )
+
+                HorizontalDivider()
+
+            }
+        }
+    }
+}
